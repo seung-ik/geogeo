ba66da5f4f41633e5693c7223c7e780c86a587b1
seung-ik <5seungik@traum.biz>
2022-04-26 14:09:24 +0900

    change js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-const widthBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-const nextEnv = require('next-env'); 
-const dotenvLoad = require('dotenv-load');
-const webpack = require('webpack');
-
-dotenvLoad();
-
-const withNextEnv = nextEnv();
-
-module.exports = { 
-	webpack(config ,{ isServer, buildId} ) {
-		config.resolve.modules.push(__dirname)
-
-		return config;  
-	}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
